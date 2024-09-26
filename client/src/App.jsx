71dc2fbaf39cef26d0d398ab6e839a38955fcaef
AfsalRoxix <mohamedafsal237@gmail.com>
2024-09-26 09:43:48 +0530

    changed skeleton design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -34,7 +34,7 @@ function App() {
     dispatch(checkAuth(token))
   },[dispatch])
 
-  if (isLoading) return <Skeleton className="w-[800] bg-black h-[600px]" />;
+  if (isLoading) return <Skeleton className="w-[800] bg-black max-h-full" />;
 
 
   // console.log(isLoading,user)
